088961e87e8a546c93ad2b98b249d779eac25113
Carlos Ariza <carlosariza7459@gmail.com>
2024-08-30 16:35:49 -0400

    add 3d map

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,27 +3,26 @@ import { Map } from '../ui/map';
 
 export default function Layout() {
     return (
-        <div className='flex h-screen '>
-            {/* Sidenav en la izquierda */}
+        <div className='flex h-screen'>
+            {/* Sidenav on the left */}
             <div className='w-72 flex-none z-10 bg-white'>
                 <SideNav />
             </div>
 
-            {/* Contenido principal */}
+            {/* Main content */}
             <div className='bg-white rounded-2xl flex-1 overflow-auto z-0 p-4'>
                 <div className="h-full bg-gradient-to-br from-custom-blue to-custom-blue-light p-8 flex flex-col gap-8 rounded-2xl">
-                    <div className="flex gap-8 flex-grow">
-                        <div className="bg-white rounded-2xl p-8 flex-grow">
-                            <Map/>
+                    <div className=" h-[300px] flex gap-8 flex-grow">
+                        <div className="bg-white rounded-2xl flex-grow">
+                            <div className="h-full w-full">
+                                <Map />
+                            </div>
                         </div>
                         <div className="bg-white rounded-2xl p-8 w-1/3"></div>
                     </div>
                     <div className="bg-white rounded-2xl p-8 flex-grow"></div>
                 </div>
             </div>
-
-
-
         </div>
     );
 }
--- src/app/ui/map.tsx
+++ src/app/ui/map.tsx
@@ -1,47 +1,82 @@
-"use client"
+"use client";
 
-import { Loader } from '@googlemaps/js-api-loader';
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react';
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+// Replace this with your Mapbox access token
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
 
 export function Map() {
-
-    const mapRef = React.useRef(null);
+    const mapRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const initMap = async () => {
-
-
-            const loader = new Loader({
-                apiKey: process.env.GOOGLE_MAPS_API_KEY!,
-                version: 'weekly'
+        if (mapRef.current) {
+            // Create the map using Mapbox GL JS
+            const map = new mapboxgl.Map({
+                container: mapRef.current, // Container ID
+                style: 'mapbox://styles/mapbox/satellite-v9', // Mapbox Satellite style URL
+                center: [-69.07215409231613, -24.269798502643763], // Initial position [lng, lat]
+                zoom: 14, // Initial zoom level
+                pitch: 70, // Pitch the map for a 3D effect
+                bearing: 90, // Rotate the map
+                antialias: true, // Improve the rendering quality of the map
             });
 
-            const { Map } = await loader.importLibrary('maps');
+            // Add 3D terrain
+            map.on('load', () => {
+                map.addSource('mapbox-dem', {
+                    type: 'raster-dem',
+                    url: 'mapbox://mapbox.terrain-rgb',
+                    tileSize: 512,
+                    maxzoom: 14,
+                });
+                map.setTerrain({ source: 'mapbox-dem' });
 
-            const position = {
-                lat: -24.269798502643763,
-                lng: -69.07215409231613
-            }
+                // Add sky layer for a more immersive experience
+                map.addLayer({
+                    id: 'sky',
+                    type: 'sky',
+                    paint: {
+                        'sky-type': 'atmosphere',
+                        'sky-atmosphere-sun': [0.0, 0.0],
+                        'sky-atmosphere-sun-intensity': 15,
+                    },
+                });
+            });
 
-            // map options
+            // Add navigation control (zoom buttons + compass)
+            map.addControl(new mapboxgl.NavigationControl());
 
-            const mapOptions: google.maps.MapOptions = {
-                center: position,
-                zoom: 17,
-                mapId: 'mapid'
-            }
+            // Coordinates for the markers
+            const markerCoordinates: [number, number][] = [
+                [-69.0834131270831, -24.2540593234207],
+                [-69.0838175921978, -24.2639819294326], 
+                [-69.0840638601929, -24.2544289147602], 
+                [-69.0864741399838, -24.2563244426373], 
+      
+            ];
 
-            const map = new Map(mapRef.current! as HTMLDivElement, mapOptions);
+            // Add markers to the map
+            markerCoordinates.forEach((coords: [number, number]) => {
+                new mapboxgl.Marker()
+                    .setLngLat(coords as LngLatLike)
+                    .addTo(map);
+            });
 
+            return () => map.remove(); // Clean up on unmount
         }
-
-        initMap();
-    }, [])
+    }, []);
 
     return (
-        <div>
-            <div style={{ height: '300px', width: '100%' }} className="" ref={mapRef} />
-        </div>
-
-    )
+        <div
+            style={{
+                height: '100%',
+                width: '100%',
+                borderRadius: '15px',
+                overflow: 'hidden',
+            }}
+            ref={mapRef}
+        />
+    );
 }
