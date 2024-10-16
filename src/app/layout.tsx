1cd458919d7a92f0c8ddf7fa2d12b97b07212d3d
Carlos Ariza <carlosariza7459@gmail.com>
2024-10-16 00:59:31 -0300

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@
 import { SessionProvider } from "next-auth/react";
 import '@/app/ui/global.css';
 import { ReactNode } from 'react';
+import { Poppins } from 'next/font/google';
+
+// Import the Poppins font
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -10,7 +17,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>
         <SessionProvider>
           {children}
