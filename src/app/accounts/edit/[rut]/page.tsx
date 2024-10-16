e6deca97040663eb160f8801f6583b4563045eff
Carlos Ariza <carlosariza7459@gmail.com>
2024-10-16 00:00:31 -0300

    update form

--- src/app/accounts/edit/[rut]/page.tsx
+++ src/app/accounts/edit/[rut]/page.tsx
@@ -1,8 +1,15 @@
-'use client';
+"use client";
 
 import { useState, useEffect } from 'react';
 import { useRouter, useParams } from 'next/navigation'; // Use useParams to get the 'rut' from the URL
 import Sidenav from '@/app/ui/dashboard/sidenav';
+import { DniInput } from '@/app/ui/components/input/dni-input';
+import { NameInput } from '@/app/ui/components/input/name-input';
+import { LastNameInput } from '@/app/ui/components/input/last-name-input';
+import { EmailInput } from '@/app/ui/components/input/email-input';
+import { RoleSelect } from '@/app/ui/components/input/role-input';
+import { CheckCircleIcon } from '@heroicons/react/24/outline'; // Importa el ícono de Heroicons
+
 
 const EditUserPage = () => {
   const router = useRouter();
@@ -16,6 +23,7 @@ const EditUserPage = () => {
     usu_estado: 'true',
   });
   const [roles, setRoles] = useState([]); // To store the roles
+  const [state, setState] = useState({ errors: { dni: [], name: [], lastName: [], email: [], role: [] } }); // Define state for errors
 
   // Fetch user data based on the 'rut' parameter
   useEffect(() => {
@@ -46,10 +54,14 @@ const EditUserPage = () => {
   }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
+    const { name, value } = e.target;
+
+    if (name && name !== 'undefined') {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -83,97 +95,65 @@ const EditUserPage = () => {
       <div className="flex-1 flex flex-col h-screen overflow-auto p-6">
         <div className="flex flex-col justify-center h-full w-full bg-gradient-to-br from-custom-blue to-custom-blue-light p-6 rounded-2xl">
           <div className="flex-1 flex items-center justify-center">
-            <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-lg h-full flex flex-col justify-between">
+            <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-lg h-full flex flex-col justify-center">
               <h1 className="text-custom-blue text-2xl font-bold text-center mb-4">Editar Usuario</h1>
 
               {/* Form content */}
-              <form onSubmit={handleSubmit} className="flex flex-col justify-between h-full">
+              <form onSubmit={handleSubmit} className="flex-1 flex flex-col justify-between">
                 <div className="flex-grow">
-                  <div className="mb-2">
-                    <label className="block mb-1 text-sm">RUT</label>
-                    <input
-                      type="text"
-                      name="usu_id_rut"
-                      value={formData.usu_id_rut}
-                      onChange={handleChange}
-                      className="w-full border px-2 py-1 text-sm rounded"
-                      required
-                      readOnly
-                    />
-                  </div>
+                  <DniInput
+                    name="usu_id_rut"
+                    id="usu_id_rut"
+                    value={formData.usu_id_rut}
+                    onChange={handleChange}
+                    state={state}
+                    readOnly
+                  />
+                  <NameInput
+                    name="usu_nombre"
+                    id="usu_nombre"
+                    value={formData.usu_nombre}
+                    onChange={handleChange}
+                    state={state}
+                  />
+                  <LastNameInput
+                    name="usu_apellido"
+                    id="usu_apellido"
+                    value={formData.usu_apellido}
+                    onChange={handleChange}
+                    state={state}
+                  />
+                  <EmailInput
+                    name="usu_correo"
+                    id="usu_correo"
+                    value={formData.usu_correo}
+                    onChange={handleChange}
+                    state={state}
+                  />
+                  <RoleSelect roles={roles} value={formData.rol_id} onChange={handleChange} state={state} />
 
-                  <div className="mb-2">
-                    <label className="block mb-1 text-sm">Nombres</label>
-                    <input
-                      type="text"
-                      name="usu_nombre"
-                      value={formData.usu_nombre}
-                      onChange={handleChange}
-                      className="w-full border px-2 py-1 text-sm rounded"
-                      required
-                    />
-                  </div>
-
-                  <div className="mb-2">
-                    <label className="block mb-1 text-sm">Apellidos</label>
-                    <input
-                      type="text"
-                      name="usu_apellido"
-                      value={formData.usu_apellido}
-                      onChange={handleChange}
-                      className="w-full border px-2 py-1 text-sm rounded"
-                      required
-                    />
-                  </div>
-
-                  <div className="mb-2">
-                    <label className="block mb-1 text-sm">Correo</label>
-                    <input
-                      type="email"
-                      name="usu_correo"
-                      value={formData.usu_correo}
-                      onChange={handleChange}
-                      className="w-full border px-2 py-1 text-sm rounded"
-                      required
-                    />
-                  </div>
-
-                  <div className="mb-2">
-                    <label className="block mb-1 text-sm">Rol</label>
-                    <select
-                      name="rol_id"
-                      value={formData.rol_id}
-                      onChange={handleChange}
-                      className="w-full border px-2 py-1 text-sm rounded"
-                      required
-                    >
-                      <option value="">Seleccionar rol</option>
-                      {roles.map((role: { rol_id: number; rol_nombre: string }) => (
-                        <option key={role.rol_id} value={role.rol_id}>
-                          {role.rol_nombre}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-
-                  {/* New Select for Activo/Desactivo */}
+                  {/* Estado Select */}
                   <div className="mb-2">
                     <label className="block mb-1 text-sm">Estado</label>
-                    <select
-                      name="usu_estado"
-                      value={formData.usu_estado}
-                      onChange={handleChange}
-                      className="w-full border px-2 py-1 text-sm rounded"
-                      required
-                    >
-                      <option value="true">Activo</option>
-                      <option value="false">Desactivo</option>
-                    </select>
+                    <div className="relative">
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <CheckCircleIcon className="h-5 w-5 text-gray-500" />
+                      </span>
+                      <select
+                        name="usu_estado"
+                        value={formData.usu_estado}
+                        onChange={handleChange}
+                        className="w-full border px-2 py-1 pl-8 text-sm rounded" // Ajustar el padding izquierdo (pl-8) para dejar espacio al ícono
+                        required
+                      >
+                        <option value="true">Activo</option>
+                        <option value="false">Desactivo</option>
+                      </select>
+                    </div>
                   </div>
                 </div>
 
-                {/* Buttons placed at the bottom */}
-                <div className="flex justify-between mt-4">
+                <div className="flex justify-between mt-3">
                   <button
                     type="button"
                     onClick={() => router.push('/accounts')}
@@ -194,7 +174,6 @@ const EditUserPage = () => {
         </div>
       </div>
     </div>
-
   );
 };
 
