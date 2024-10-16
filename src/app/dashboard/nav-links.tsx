35360385678b6f96450f8b9cc0df3fad23c75a1e
Carlos Ariza <carlosariza7459@gmail.com>
2024-10-16 01:16:22 -0300

    protect routes

--- src/app/dashboard/nav-links.tsx
+++ src/app/dashboard/nav-links.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
@@ -11,7 +12,11 @@ const links = [
   { name: 'Inicio', href: 'dashboard' },
   { name: 'Pozos', href: 'nodes' },
   { name: 'Canales', href: 'channels' },
+];
+
+const adminLinks = [
   { name: 'Carga de Datos', href: 'uploads' },
+  { name: 'Cuentas', href: 'accounts' },
 ];
 
 const NavLinks: React.FC<NavLinksProps> = ({ role }) => {
@@ -36,22 +41,23 @@ const NavLinks: React.FC<NavLinksProps> = ({ role }) => {
         </Link>
       ))}
 
-      {/* Conditionally render the "Cuentas" link only for Admins */}
-      {role === 'Admin' && (
-        <Link
-          key="Cuentas"
-          href="/accounts"
-          className={clsx(
-            'my-2 flex h-[48px] grow items-center justify-center rounded-md p-3 text-sm text-gray-700 transition-colors hover:bg-custom-blue hover:text-white md:flex-none md:justify-start md:p-2 md:px-3',
-            {
-              'bg-custom-blue text-white font-semibold':
-                pathname === '/accounts',
-            }
-          )}
-        >
-          <p className="hidden md:block">Cuentas</p>
-        </Link>
-      )}
+      {/* Conditionally render the "Carga de Datos" and "Cuentas" links only for Admins */}
+      {role === 'Admin' &&
+        adminLinks.map((link) => (
+          <Link
+            key={link.name}
+            href={'/' + link.href}
+            className={clsx(
+              'my-2 flex h-[48px] grow items-center justify-center rounded-md p-3 text-sm text-gray-700 transition-colors hover:bg-custom-blue hover:text-white md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-custom-blue text-white font-semibold':
+                  pathname === '/' + link.href,
+              }
+            )}
+          >
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        ))}
     </div>
   );
 };
